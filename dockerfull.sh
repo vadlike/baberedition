be5ef10964afc9229d1e3603f94ae56cf515fb0b
vadlike <33003220+vadlike@users.noreply.github.com>
2021-06-03 18:04:58 +0300

    Update dockerfull.sh

--- dockerfull.sh
+++ dockerfull.sh
@@ -24,3 +24,6 @@ sudo add-apt-repository \
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&
     sudo chmod +x /usr/local/bin/docker-compose &&
     sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+# portainer
+docker run -d -p 22221:9000 -p 8000:8000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v /docker/docker/host/data:/data portainer/portainer
