8f22c11747df87d0a19dc91572cfe3b468943b5a
vadlike <33003220+vadlike@users.noreply.github.com>
2021-06-13 23:39:22 +0300

    Update install_wireguard_debian10.sh

--- install_wireguard_debian10.sh
+++ install_wireguard_debian10.sh
@@ -1,11 +1,9 @@
-apt-get update -y && apt-get upgrade -y
-
-apt update && apt install sudo openresolv -y
-
-echo "deb http://deb.debian.org/debian/ unstable main" > /etc/apt/sources.list.d/unstable.list
-
-printf 'Package: *\nPin: release a=unstable\nPin-Priority: 90\n' > /etc/apt/preferences.d/limit-unstable
+echo 'deb http://ftp.debian.org/debian buster-backports main' | sudo tee /etc/apt/sources.list.d/buster-backports.list
 
 apt update
 
-apt install -y wireguard
+apt install -y wireguard sudo
+
+
+
+
