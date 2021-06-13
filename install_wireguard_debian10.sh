ef8894f9b1caa6236711119ec8dc0432b3bc734e
vadlike <33003220+vadlike@users.noreply.github.com>
2021-06-13 23:57:38 +0300

    Update install_wireguard_debian10.sh

--- install_wireguard_debian10.sh
+++ install_wireguard_debian10.sh
@@ -1,10 +1,11 @@
 apt-get update -y && apt-get upgrade -y
 
-echo 'deb http://ftp.debian.org/debian buster-backports main' | sudo tee /etc/apt/sources.list.d/buster-backports.list
+echo 'deb http://deb.debian.org/debian buster-backports main' | sudo tee /etc/apt/sources.list.d/buster-backports.list
 
 apt update
 
 apt install -y wireguard sudo openresolv
+
 
 
 
