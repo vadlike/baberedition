a2ca46f13bc6ee6e5db95fd184733c06559bd011
vadlike <33003220+vadlike@users.noreply.github.com>
2021-06-14 21:28:46 +0300

    Update genSSHkeyFORroot.sh

--- genSSHkeyFORroot.sh
+++ genSSHkeyFORroot.sh
@@ -2,6 +2,8 @@ cd
 
 cd .ssh
 
+rm authorized_keys
+
 ssh-keygen -t rsa -b 4096
 
 cat id_rsa.pub >> authorized_keys
@@ -9,3 +11,5 @@ cat id_rsa.pub >> authorized_keys
 chmod 0600 authorized_keys
 
 rm id_rsa.pub
+
+rm genSSHkeyFORroot.sh
