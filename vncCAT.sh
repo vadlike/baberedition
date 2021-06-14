31ab3e171a3860faaab0bca3ccc3d30868646198
vadlike <33003220+vadlike@users.noreply.github.com>
2021-06-14 12:42:12 +0300

    Create vncCAT.sh

---
+++ vncCAT.sh
@@ -0,0 +1,35 @@
+#disable systemd-logind и getty
+
+systemctl disable getty@tty1
+
+systemctl disable getty-static
+
+echo "NAutoVTs=0
+ReserveVT=0" > /etc/systemd/logind.conf
+
+#add Nyan cat
+
+apt-get install nyancat -y
+
+
+echo "[Unit]
+Description=nyancat on tty1
+After=graphical.target
+
+[Service]
+Type=simple
+ExecStartPre=/bin/sleep 5
+ExecStart=/usr/bin/nyancat -snI
+ExecStop=/bin/kill -HUP ${MAINPID}
+StandardInput=tty
+StandardOutput=tty
+TTYPath=/dev/tty1
+Restart=always
+RestartSec=2
+
+[Install]
+WantedBy=graphical.target" > /etc/systemd/system/nyancat-tty.service
+
+systemctl enable nyancat-tty.service
+
+
