ce28a39288ad5326c3cdce57530fb0ed12ddfa59
vadlike <33003220+vadlike@users.noreply.github.com>
2021-06-15 00:55:12 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,46 +1,4 @@
 #!/bin/bash
-clear
-echo " ###############################################################"
-echo " # Setup server config Netcup/Hetzner Ubuntu 18.04 and above   #"
-echo " # Setup server config Netcup/Hetzner Debian 10                #"
-echo " ###############################################################"
-echo ""
-echo ""
-echo "To EXIT this script press  [ENTER]"
-echo 
-read -p "To RUN this script press  [Y]" -n 1 -r
-echo
-echo
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    exit 1
-fi
-
-if [[ "$EUID" -ne 0 ]]; then
-	echo "Sorry, you need to run this as root"
-	exit 1
-fi
-
-if [[ -e /etc/debian_version ]]; then
-      echo "Debian Distribution"
-      else
-      echo "This is not a Debian Distribution."
-      exit 1
-fi
-
-
-
-echo "Set network config"
-read -p "Your hostname :" -e -i remotehost hostnamex
-hostnamectl set-hostname $hostnamex
-if [ -f "/etc/netplan/50-cloud-init.yaml" ]; then
-    nano /etc/netplan/50-cloud-init.yaml
-fi
-if [ -f "/etc/network/interfaces.d/50-cloud-init.cfg" ]; then
-   nano /etc/network/interfaces.d/50-cloud-init.cfg
-fi
-# fix resolf conf
-sed -i 's/127.0.1.1/#127.0.1.1/' /etc/hosts
-echo '127.0.1.1       $hostnamex' >> /etc/hosts
-
-clear
+HOSTNAME=""
+echo $HOSTNAME > /etc/hostname
+HOST=$HOSTNAME
