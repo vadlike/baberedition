3ee8dfc4b178c7c8f262dc1c7025151bd4f01b3a
vadlike <33003220+vadlike@users.noreply.github.com>
2021-06-15 01:11:09 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,5 +3,4 @@ NEW_HOSTNAME=$1
 echo $NEW_HOSTNAME > /proc/sys/kernel/hostname
 sed -i 's/127.0.1.1.*/127.0.1.1\t'"$NEW_HOSTNAME"'/g' /etc/hosts
 echo $NEW_HOSTNAME > /etc/hostname
-service hostname start
-su $SUDO_USER -c "xauth add $(xauth list | sed 's/^.*\//'"$NEW_HOSTNAME"'\//g' | awk 'NR==1 {sub($1,"\"&\""); print}')"
+
