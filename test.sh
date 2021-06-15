6bfa572410558931935920d20ee6fc0a5370c358
vadlike <33003220+vadlike@users.noreply.github.com>
2021-06-15 11:01:40 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
-NEW_HOSTNAME=$1
-echo $NEW_HOSTNAME > /proc/sys/kernel/hostname
-sed -i 's/127.0.1.1.*/127.0.1.1\t'"$NEW_HOSTNAME"'/g' /etc/hosts
-echo $NEW_HOSTNAME > /etc/hostname
+#! /bin/bash
 
+echo "Enter name for this host"
+read node
+sed '1d' /etc/hostname
+echo $node > /etc/hostname
